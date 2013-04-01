dd7885828b34e6fb0e0e3e3ca8d31262e71c3121
Pablo Gonzalez <pbglezc@gmail.com>
2013-04-01 20:01:39 +0000

    test posts

--- occipital_server.js
+++ occipital_server.js
@@ -7,22 +7,32 @@ Ne.Class(Occipital, 'Server')({
     prototype : {
         http : require('http'),
         init : function init (config) {
+            var app = this;
+
             Object.keys(config).forEach(function (property) {
                 this[property] = config[property];
             }, this);
 
-            this.server = this.http.createServer(this.handleRequest);
+            this.server = this.http.createServer(function (request, response) {
+                app.handleRequest.call(this, app, request, response);
+            });
         },
 
         listen : function listen (port) {
             this.server.listen(port);
         },
 
-        handleRequest : function handleRequest (request, response) {
-            console.log('request received');
-            response.writeHead(200, {"Content-Type": "text/plain"});
-            console.log('hola');
-            response.write('hola');
+        handleRequest : function handleRequest (app, request, response) {
+            console.log(request.body);
+            if (request.method === "POST") {
+
+            }
+            return app.notFound(request, response);
+        },
+
+        notFound : function notFound (request, response) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write('Original image not found.');
             response.end();
         }
     }
